f755aedbc2005850d8b84f1f9d28379566580363
StellaR0511 <81784995+StellaR0511@users.noreply.github.com>
2021-11-22 20:37:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+ function addUser(){
+     player_1 = document.getElementById("player_1_name_input").value;
+     player_2 = document.getElementById("player_2_name_input").value;
+     localStorage.setItem("player1_name",player_1);
+     localStorage.setItem("player2_name",player_2);
+     window.location = "game_page.html";
+
+ }
